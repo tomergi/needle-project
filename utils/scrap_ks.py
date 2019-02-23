9bf306be0eb85129937f26b6e304630d163cf178
Ben Asaf <ToBenAsaf@Gmail.com>
2019-02-23 17:34:38 +0200

    working on scrapping for Dr.Kickstarter

--- utils/scrap_ks.py
+++ utils/scrap_ks.py
@@ -1,10 +1,19 @@
 from bs4 import BeautifulSoup
 import os
-import json
-import urllib.request, urllib.parse
+import pandas as pd
+import numpy as np
+import urllib.request
+import urllib.parse
 from time import sleep
 import re
 import random
+
+
+CURRENCY_DICT = {
+    '€': 1.13,
+    '£': 1.31,
+}
+
 
 CURRENT_DIR = os.path.split(__file__)[0]
 
@@ -13,11 +22,15 @@ MIN_WAIT_TIME, MAX_WAIT_TIME = (5, 5)
 
 # This supposed to capture (Updates <Num>) | (Comments <Num>)
 RE_TABULAR_SECTION = re.compile("\s+(?:[A-Za-z]+)\s+(\d+)\s+")
-RE_GOAL = re.compile("pledged\sof\s\$([\d,]+)\sgoal")
+PARSE_MONEY_STR = "([$¢£¤¥֏؋৲৳৻૱௹฿៛\u20a0-\u20bd\ua838\ufdfc\ufe69\uff04\uffe0\uffe1\uffe5\uffe6])([\d,]+)"
+RE_PARSE_MONEY = re.compile(PARSE_MONEY_STR)
+RE_GOAL = re.compile("pledged\sof\s"+PARSE_MONEY_STR+"\sgoal")
 RE_PLEDGED = re.compile("\$\s*([\d,]+)")
 RE_BACKERS = re.compile("([\d,]+)\s*backers")
 RE_TIME_LEFT = re.compile("(\w+)\s*(\w+)(?:to\sgo)")
 
+
+COUNTRY_DATAFRAME = pd.read_csv("./world-cities.csv")
 # def parse_card(soup):
 #     attrs = {}
 #     stats_pat = re.compile('(.*)\s+made\s+it\s+\|\s+(.*)\s+reviews\s+\|\s+(.*)\s+photos')
@@ -60,17 +73,18 @@ def load_page(url):
     rand_time = random.randint(MIN_WAIT_TIME, MAX_WAIT_TIME)
     sleep(rand_time)
     with urllib.request.urlopen(url) as response:
-        html = response.read()
-    soup = BeautifulSoup(html, 'html.parser')
+        raw_html = response.read()
+    soup = BeautifulSoup(raw_html, 'html.parser')
     return soup
 
 
-def extract_title(soup):
-    return soup.find_all('h2', attrs={'class': 'type-24 type-28-sm type-38-md navy-700 medium mb3'})[0].text
+def extract_title_and_description(soup):
+    title_desc_cont = soup.find_all('div', attrs={"class": "col-20-24 block-md order-2-md col-lg-14-24"})
+    elems = list(title_desc_cont[0].find_all())[:-1]
+    title = elems[0].text
+    description = elems[1].text
+    return title, description
 
-
-def extract_description(soup):
-    return None
 
 def extract_num_updates(soup):
     num_updates_text = soup.find_all('a', attrs={'class': 'js-load-project-updates'})[0].text
@@ -92,11 +106,20 @@ def extract_num_comments(soup):
         raise ValueError("Could not find number of comments!")
 
 
-def extract_usa_state(soup):
+def extract_country_subcountry_city(soup):
     filter_lambda = lambda s: s.startswith("https://www.kickstarter.com/discover/places/")
-    state_text = soup.find_all('a', attrs={'href': filter_lambda})[0].text
-    return state_text
+    cand_city, cand_subcountry = tuple(map(str.strip, soup.find_all('a', attrs={'href': filter_lambda})[0].text.split(',')))
+    result = COUNTRY_DATAFRAME.loc[COUNTRY_DATAFRAME['name'] == cand_city]
 
+    city = result['name'].values[0]
+    subcountry = result['subcountry'].values[0]
+    country = result['country'].values[0]
+    return country, subcountry, city
+
+def extract_category_subcategory(soup):
+    filter_lambda = lambda s: s.startswith("https://www.kickstarter.com/discover/categories/")
+    result = soup.find_all('a', attrs={'href': filter_lambda})[0]
+    return result.text
 
 def extract_creator(soup):
     creator = soup.find_all('span', attrs={'class': "soft-black ml2 ml0-md break-word"})[0].text
@@ -109,14 +132,38 @@ def extract_num_projects_by_creator(soup):
     return num_projects_text[0:end_idx]
 
 
+def parse_money(currency, amount):
+    amount = amount.replace(',', '')
+    amount = float(amount)
+    if currency == '$':
+        amount = amount
+    elif currency == '€':
+        amount = CURRENCY_DICT['€']*amount
+    elif currency == '£':
+        amount = CURRENCY_DICT['£'] * amount
+    else:
+        raise ValueError("Non supported currency based project")
+    return np.round(amount, decimals=2)
 
 def extract_goal(soup):
     goal_text = soup.find_all('span', attrs={'class': "inline-block-sm hide"})[0].text
     m = RE_GOAL.match(goal_text)
     if m:
-        return m.group(1)
+        currency = m.group(1)
+        amount = m.group(2)
+        return parse_money(currency, amount)
     else:
-        raise ValueError("Could not find goal! ('pledged of...)")
+        raise ValueError("Could not find goal! ('pledged of...).")
+
+def extract_num_pledged(soup):
+    num_pledged_text = soup.find_all("span", attrs={'class': "ksr-green-700 inline-block medium type-16 type-24-md"})[0].text
+    m = RE_PARSE_MONEY.match(num_pledged_text)
+    if m:
+        currency = m.group(1)
+        amount = m.group(2)
+        return parse_money(currency, amount)
+    else:
+        raise ValueError("Could not find pledged amount.")
 
 
 def extract_num_backers(soup):
@@ -128,45 +175,30 @@ def extract_num_backers(soup):
         raise ValueError("Could not find number of backers!")
 
 
-def extract_num_pledged(soup):
-    num_pledged_text = soup.find_all("span", attrs={'class': "ksr-green-700 inline-block medium type-16 type-24-md"})[0].text
-    m = RE_PLEDGED.match(num_pledged_text)
-    if m:
-        return m.group(1)
-    else:
-        raise ValueError("Could not find pledged amount.")
-
-
 def extract_time_left(soup):
-    elem = soup.find_all('div', attrs={"class": "ml5 ml0-lg"})
+    elem = soup.find_all('p', attrs={"class": "mb3 mb0-lg type-12"})[0]
     return elem.text
+
+
+def extract_project_we_love(soup):
+    result = soup.find_all('div', attrs={'class': "col-full"})
+    print(result)
 
 
 def parse_page(soup):
     item = {}
-    item['title'] = extract_title(soup)
-    item['desc'] = extract_description(soup)
+    item['title'], item['description'] = extract_title_and_description(soup)
     item['num_updates'] = extract_num_updates(soup)
     item['num_comments'] = extract_num_comments(soup)
-    item['usa_state'] = extract_usa_state(soup)
+    item['country'], item['subcountry'], item['city'] = extract_country_subcountry_city(soup)
     item['creator'] = extract_creator(soup)
     item['num_projects_by_creator'] = extract_num_projects_by_creator(soup)
     item['num_backers'] = extract_num_backers(soup)
-    item['goal'] = extract_goal(soup)
     item['num_pledged'] = extract_num_pledged(soup)
-    item['time_left'] = extract_time_left(soup)
-
-
-    # num_recipes = len(recipe_cards)
-    # for i, x in enumerate(recipe_cards, 1):
-    #     print('\r\tRecipe: %d / %d' % (i, num_recipes), end='')
-    #     try:
-    #         parsed = parse_thumbnail(x)
-    #         items[parsed['Title']] = parsed
-    #     except Exception as e:
-    #         pass
-    #         # print("Failed to parse card. Thrown %s" % e)
-    # print()  # To make a new line and reset the carriage return
+    item['goal'] = extract_goal(soup)
+    item['timeleft'] = extract_time_left(soup)
+    item['category'], item['subcategory'] = extract_category_subcategory(soup)
+    item['project_we_love'] = extract_project_we_love(soup)
     return item
 
 
@@ -197,5 +229,7 @@ def scrap_from_file(fp):
 
 if __name__ == '__main__':
     # run(base_url=base_url)
-    soup = load_page("https://www.kickstarter.com/projects/13/1000056157")
+    link1 = "https://www.kickstarter.com/projects/13/1000056157"
+    link2 = "https://www.kickstarter.com/projects/horriblegames/alonetm-2nd-print-run-with-new-contents-and-locali?ref=discovery_category"
+    soup = load_page(link2)
     parsed = parse_page(soup)
